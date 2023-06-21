6a30a3c7bba2cf4ee0d22d0ac32ed907f5e351d8
BasmaKuhail <basmakuhail2003@gmail.com>
2023-06-22 00:45:31 +0300

    TODO 4: Student class constructor

--- final_project.py
+++ final_project.py
@@ -36,8 +36,16 @@ class Student:
     # student_age (user input)
     # student_number (user_input)
     # courses_list (List of Course Objects)
-    def __init__(self):
-        pass
+    def __init__(self, student_name, student_age, student_number, courses_list):
+        # update total_std_count
+        Student.total_std_count += 1
+
+        self.student_id = str(uuid.uuid4())
+        self.student_name = student_name
+        self.student_age = student_age
+        self.student_number = student_number
+        self.courses_list = courses_list
+
 
     # TODO 5 define a method to enroll new course to student courses list
 
